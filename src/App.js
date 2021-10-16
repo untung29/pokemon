b7f19b824ac7a5f4c3495088bb752876f52b6329
Untung <u.untung0029@gmail.com>
2021-10-16 17:42:09 +0700

    Add routing library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from "./logo.svg";
 
+// Router Functionality
+import { Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomeComponent} />
+        <Route exact path="/detail" component={DetailComponent} />
+      </Switch>
+    </div>
+  );
 }
+
+const HomeComponent = () => {
+  return <h1>HomeComponent</h1>;
+};
+
+const DetailComponent = () => {
+  return <h1>DetailComponent</h1>;
+};
 
 export default App;
